fb7cc15e3d74d3be80fe6b644287be307884476d
van-kevin suy <33053798+vankevinsuy@users.noreply.github.com>
2021-01-25 17:31:47 +0100

    création de containeur pour automatiser les updates

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.8.2
+
+WORKDIR /usr/src/app
+
+COPY . .
+
+RUN pip install -r requirements.txt
+
+
+CMD ["python" , "main.py"]
---
+++ main.py
@@ -0,0 +1,9 @@
+from Fillers import FillMongoDB
+
+FillMongoDB.UpdateAnime()
+
+FillMongoDB.UpdateManga()
+
+FillMongoDB.UpdateEpisodes()
+
+FillMongoDB.UpdateChapters()
